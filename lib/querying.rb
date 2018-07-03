69dd4c01a4ebf049012943412326152780377f7f
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-03 16:33:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series WHERE species = "human"
   ORDER BY SUM(species)
-  LIMIT 1"
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
